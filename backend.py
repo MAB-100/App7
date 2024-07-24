1c094e6feab0a0b04819e5a3477d9f8550a59b16
MAB-100 <shahabbas661@gmail.com>
2024-07-24 14:04:57 +0500

    backend initial work

---
+++ backend.py
@@ -0,0 +1,5 @@
+
+
+def get_data(place , days, selection):
+    
+    return data
--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import streamlit as st
+import plotly.express as px
+from backend import get_data
+
 
 st.title('Weather Forecast for the next days')
 
-col1 = st.columns(1)
 
-with col1[0]:
-    place = st.text_input('Place', placeholder='Type the city name here')
-    days = st.slider('Forcast Days', 1, 5)
-    selection = st.selectbox('Select data to view', ('Temperature', 'Sky'))
-    st.subheader(f"{selection} for the next {days} days in {place}")
+place = st.text_input('Place', placeholder='Type the city name here')
+days = st.slider('Forcast Days', 1, 5)
+selection = st.selectbox('Select data to view', ('Temperature', 'Sky'))
+st.subheader(f"{selection} for the next {days} days in {place}")
 
+
+def get_data(days):
+    dates = ['2021-01-01', '2021-01-02', '2021-01-03', '2021-01-04', '2021-01-05']
+    temperatures = [10, 20, 30, 4, 23]
+    temperatures = [days* i for i in temperatures]
+    return dates , temperatures
+
+
+data = get_data(place, days, selection)
+
+
+d , t = get_data(days)
+
+
+figure = px.line(x=d, y=t, title='Temperature Forecast', 
+                 labels={'x': 'Date', 'y': 'Temperature'})
+st.plotly_chart(figure)
