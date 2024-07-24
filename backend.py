892d0d7e1c9920fefbdc2f5a37da78059beb7360
MAB-100 <shahabbas661@gmail.com>
2024-07-24 17:24:29 +0500

    open weather api implemented

--- backend.py
+++ backend.py
@@ -1,5 +1,13 @@
+import requests
 
 
-def get_data(place , days, selection):
-    
+API_key = "ad6d8a409cd3e137cfa844b32487b7be"
+
+def get_data(place , days = None, selection = None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_key}"
+    respomse = requests.get(url)
+    data = respomse.json()
     return data
+
+if __name__ == "__main__":
+    print(get_data(place="London"))
